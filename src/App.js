82b70d678166aec2d99eeb63587d9080b6d12ba9
Cristiano Santos <cristianossd@gmail.com>
2018-03-18 12:48:03 -0300

    Travis-CI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import {Helmet} from "react-helmet";
-import autoBind from 'react-autobind';
+import React, { Component } from "react";
+import { Helmet } from "react-helmet";
+import autoBind from "react-autobind";
 
-import LeaderboardTable from './components/leaderboard-table/';
-import logo from './assets/logo.png';
+import LeaderboardTable from "./components/leaderboard-table/";
+import logo from "./assets/logo.png";
 
 class App extends Component {
   constructor(props) {
     super(props);
     autoBind(this);
     this.state = {
-      category: 'isMen',
-      filterText: '',
+      category: "isMen",
+      filterText: ""
     };
   }
 
   toggleCategory(event) {
     const { target } = event;
-    const attr = target.getAttribute('dataref');
+    const attr = target.getAttribute("dataref");
 
     this.setState({ category: attr });
   }
@@ -29,10 +29,12 @@ class App extends Component {
   render() {
     const { category, filterText } = this.state;
     const boostrap = {
-      rel: 'stylesheet',
-      href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-      integrity: 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm',
-      crossorigin: 'anonymous',
+      rel: "stylesheet",
+      href:
+        "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+      integrity:
+        "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm",
+      crossorigin: "anonymous"
     };
 
     return (
@@ -40,30 +42,45 @@ class App extends Component {
         <Helmet
           defaultTitle="2018 Open Crossfit Leaderboard - Bahia"
           meta={[
-            { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
-            { name: 'description', content: '2018 Open Crossfit Leaderboard Bahia' },
+            {
+              name: "viewport",
+              content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+            },
+            {
+              name: "description",
+              content: "2018 Open Crossfit Leaderboard Bahia"
+            }
           ]}
-          link={[ boostrap ]}
+          link={[boostrap]}
         />
 
         <div className="container">
           <div className="py-5 text-center">
-            <img className="d-block mx-auto mb-4" src={logo} alt="Open Crossfit Bahia" />
+            <img
+              className="d-block mx-auto mb-4"
+              src={logo}
+              alt="Open Crossfit Bahia"
+            />
             <h2>2018 Open Crossfit Leaderboard - Bahia</h2>
             <p className="lead">Acompanhe o leaderboard dos atletas baianos</p>
 
             <div className="row justify-content-md-center">
               <div className="col-md-auto col-lg-4">
-
                 <div className="btn-group">
-                  <button className={`btn btn-primary ${ category === 'isMen' ? 'active' : '' }`}
+                  <button
+                    className={`btn btn-primary ${
+                      category === "isMen" ? "active" : ""
+                    }`}
                     dataref="isMen"
                     onClick={this.toggleCategory}
                   >
                     MASCULINO
                   </button>
 
-                  <button className={`btn btn-primary ${ category === 'isWomen' ? 'active' : '' }`}
+                  <button
+                    className={`btn btn-primary ${
+                      category === "isWomen" ? "active" : ""
+                    }`}
                     dataref="isWomen"
                     onClick={this.toggleCategory}
                   >
@@ -81,7 +98,6 @@ class App extends Component {
                     placeholder="Pesquise por atleta ou box"
                   />
                 </div>
-
               </div>
             </div>
           </div>
@@ -89,16 +105,26 @@ class App extends Component {
           <LeaderboardTable category={category} filterText={filterText} />
 
           <footer className="my-5 pt-5 text-muted text-center text-small">
-            <p className="mb-1">&copy; 2018 Open Crossfit Games Leaderboard Bahia</p>
+            <p className="mb-1">
+              &copy; 2018 Open Crossfit Games Leaderboard Bahia
+            </p>
             <ul className="list-inline">
               <li className="list-inline-item">
-                <a href="https://github.com/cristianossd" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://github.com/cristianossd"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Cristiano Santos
                 </a>
               </li>
 
               <li className="list-inline-item">
-                <a href="https://github.com/joaopcanario" target="_blank" rel="noopener noreferrer">
+                <a
+                  href="https://github.com/joaopcanario"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   João Paulo Canário
                 </a>
               </li>
